52f11507df116c5ec5d6ce689bceb0b2b4c1c804
Donatas Noreika <donoras@gmail.com>
2022-06-07 12:01:59 +0300

    added balance

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ zurnalas = []
 
 
 while True:
-    pasirinkimas = int(input("Pasirinkite:\n1 - įvesti pajamas\n2 - įvesti išlaidas\n3 - peržiūrėti visus įrašus\n4 - išeiti iš programos\n"))
+    pasirinkimas = int(input("Pasirinkite:\n1 - įvesti pajamas\n2 - įvesti išlaidas\n3 - peržiūrėti visus įrašus\n4 - parodyti balansą\n5 - išeiti iš programos\n"))
     if pasirinkimas == 1:
         suma = float(input("Įveskite pajamų sumą"))
         irasas = PajamuIrasas(suma)
@@ -19,5 +19,13 @@ while True:
         for irasas in zurnalas:
             print(irasas)
     if pasirinkimas == 4:
+        suma = 0
+        for irasas in zurnalas:
+            if type(irasas) is PajamuIrasas:
+                suma += irasas.suma
+            if type(irasas) is IslaiduIrasas:
+                suma -= irasas.suma
+        print("Balansas:", suma)
+    if pasirinkimas == 5:
         print("Viso gero")
         break
